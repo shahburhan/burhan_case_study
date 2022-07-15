9afd36dd6bf594b1ac85616fe1b6f5d541aa496e
Burhan Shah <sb.shahburhan@gmail.com>
2022-07-15 11:14:00 +0530

    Init app, models and migrations

---
+++ app/Models/Cart.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    /**
+     * Set the model to exclude timestamp columns for create and update
+     *
+     * @var boolean
+     */
+    public $timestamps = false;
+}
---
+++ database/migrations/2022_07_14_193359_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('name');
+            $table->float('price');
+            $table->bigInteger('category_id');
+            $table->text('description');
+            $table->string('avatar');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
